d37bcb370d96c5826f92046ead9f31fd2acbd038
smldms <97317400+smldms@users.noreply.github.com>
2022-09-10 20:28:05 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,90 @@
+let cnv;
+let formResolution = 3;
+let stepSize = 3;
+let distortionFactor = 5;
+let initRadius = 0;
+let centerX;
+let centerY;
+let x = [];
+let y = [];
+let xX;
+let yY;
+let offX = Math.random()
+let offY = Math.random()
+let distOff = Math.random()
+let globalSize = 800;
+let filled = true;
+let freeze = false;
+
+function setup() {
+    cnv = createCanvas(windowWidth, windowHeight);
+    cnv.position(0, 0)
+    cnv.style('z-index', '-99')
+    // init shape
+    centerX = width / 2;
+    centerY = height / 2;
+    let angle = 0;
+    for (let i = 0; i < formResolution; i++) {
+        x.push(cos(angle * i) * initRadius);
+        y.push(sin(angle * i) * initRadius);
+    }
+
+    stroke(255, 150);
+    strokeWeight(1);
+    xX = width / 2
+    yY = height / 2
+    background(255);
+}
+
+function draw() {
+    push()
+    xX = map(noise(offX), 0, 1, 0, width)
+    yY = map(noise(offY), 0, 1, 0, height)
+    distortionFactor = Math.round(map(noise(distOff), 0, 1, 1, 10));
+    drawingContext.shadowOffsetX = 0;
+    drawingContext.shadowOffsetY = -3;
+    drawingContext.shadowBlur = distortionFactor;
+    drawingContext.shadowColor = color(0,150);
+    // speed
+    // centerX += (mouseX - centerX) * 0.025;
+    // centerY += (mouseY - centerY) * 0.025;
+    // print(mouseX, mouseY)
+    centerX += (xX - centerX) * 0.5;
+    centerY += (yY - centerY) * 0.5;
+    // calculate new points
+    for (let i = 0; i < formResolution; i++) {
+        x[i] += random(-stepSize, stepSize);
+        y[i] += random(-stepSize, stepSize);
+        // uncomment the following line to show position of the agents
+        // ellipse(x[i] + centerX, y[i] + centerY, 5, 5);
+    }
+
+    if (filled) {
+        fill(map(noise(offY), 0,1,255,255));
+    } else {
+        noFill();
+    }
+    beginShape();
+    // first controlpoint
+    curveVertex(x[formResolution - 1] + centerX, y[formResolution - 1] + centerY);
+    for (let i = 0; i < formResolution; i++) {
+        curveVertex(x[i] + centerX, y[i] + centerY);
+    }
+    curveVertex(x[0] + centerX, y[0] + centerY);
+    // end controlpoint
+    curveVertex(x[1] + centerX, y[1] + centerY);
+    endShape();
+
+    offX += 0.0025;
+    offY += 0.00125;
+    distOff += 0.08005;
+}
+
+function mousePressed() {
+    clear();
+    background(255);
+  }
+
+  function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+  }
---
+++ sketch2.js
@@ -0,0 +1,81 @@
+let xoff = 0.01
+let yoff = 0.02
+let noiseY, noiseX;
+let zoneMax, zoneMin;
+let sclXVal; let fac;
+
+function setup() {
+
+    cnv = createCanvas(windowWidth, windowHeight);
+    cnv.position(0, 0)
+    cnv.style('z-index', '-99')
+    rectMode(CENTER)
+    zoneMax = height*0.15
+    zoneMin = height*0.90
+    sclXVal = random(10,100)
+    fac = random(0.05,0.25)
+}
+
+function draw() {
+    background(255, 25)
+    noiseY = map(noise(yoff), 0, 1, -10, 10)
+    noiseX = map(noise(xoff), 0, 1, -5, 5)
+    noiser()
+    xoff += 0.00085
+    yoff += 0.00025
+}
+
+function noiser() {
+    let l1 = {
+        maxH: zoneMax,
+        minH: zoneMin,
+        relief: 0.025,
+        clr1: color(10),
+        clr2: color(255),
+        dens: height,
+        factor: fac,
+        sclX: sclXVal,
+        sclY: 10 + noiseX,
+    }
+    push()
+    // drawingContext.filter = 'contrast(1.4) drop-shadow(20px 20px 10px #000)';
+    terrain(...Object.values(l1));
+    pop()
+}
+
+function terrain(maxH, minH, relief, clr1, clr2, dens, factor, sclX, sclY) {
+    // translate(-width / 2, -height / 2)
+    for (let y = maxH; y < minH; y += sclY) {
+        let row = [];
+        let xoff = 0.01;
+        row.push(createVector(0, y));
+        for (let x = 0; x < width + sclX; x += sclX) {
+            strokeWeight(0.25)
+            let n = noise(x * relief, y * relief);
+            let ampli = map(y, 0, height, factor, 0) * dens;
+            let off = y + map(n, 0, 1, -ampli, ampli)
+            row.push(createVector(x, off));
+        }
+        stroke(255)
+        row.push(createVector(width, height));
+        row.push(createVector(0, height));
+        let lvl = map(y, maxH, height, 0, 1.5);
+        let c = lerpColor(color(clr1), color(clr2), lvl);
+        // noFill()
+        fill(c)
+        beginShape();
+        for (let v of row) {
+            vertex(v.x, v.y);
+        }
+        endShape(CLOSE);
+    }
+}
+
+function mousePressed() {
+    clear();
+    background(255);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
